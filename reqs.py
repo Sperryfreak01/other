c7d02db1a26dc6679f4864e173080265a9b07989
Pete Friedhoff <pete.friedhoff@gmail.com>
2015-07-22 16:22:03 -0700

    got requests working

--- reqs.py
+++ reqs.py
@@ -1,14 +1,43 @@
-# -*- coding: utf-8 -*-
 """
-Created on Tue Jul 21 21:02:08 2015
-
-@author: Pete
+Script to pull data from the API for processing
 """
+import requests, json
 
-import requests
+"""
+Google Directions API example
+"""
+# Refer to: https://developers.google.com/maps/documentation/directions/intro#DirectionsRequests
+# url = 'https://maps.googleapis.com/maps/api/directions/json' 
 
-url = 'https://maps.googleapis.com/maps/api/directions/json'
-query = {'origin': 'Toronto', 'destination': 'Montreal', 'key':'AIzaSyAfY4klBTbZf16oY_hcRcH2Cr4XjEFhJHI'}
+# params = {
+# 'origin': 'Toronto',
+# 'destination': 'Montreal',
+# 'key':'AIzaSyAfY4klBTbZf16oY_hcRcH2Cr4XjEFhJHI'
+# }
 
-r = requests.get(url, params = query)
-print (r.status_code)
+# r = requests.get(url, params = params)
+# data = r.json()
+# print (r.status_code)
+
+
+"""
+Arena PLM API
+"""
+url = "https://api.arenasolutions.com/v1/"
+
+# Login first
+endpoint = 'login'
+headers = {"content-type": "application/json"}
+body = {"email": "pfriedhoff@nextracker.com", "password": "Spicyworkpass2015", "workspaceId": "896561705"}
+r = requests.post(url + endpoint, headers = headers, data = json.dumps(body))
+login_data = r.json()
+
+# Query Items
+endpoint = 'items/categories'
+headers["arena_session_id"] = login_data["arenaSessionId"]
+r = requests.get(url + endpoint, headers = headers)
+items_data = r.json()
+
+# Logout
+r = requests.get('https://api.arenasolutions.com/v1/logout', headers = headers)
+print ('Logout code is: %s' % r.status_code)
