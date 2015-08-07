3f17559cb980d4822c477026c8edb271a074d05f
Pete Friedhoff <pete.friedhoff@gmail.com>
2015-08-07 14:41:13 -0700

    some general aws cli notes

---
+++ Notes/aws_notes.py
@@ -0,0 +1,21 @@
+"""
+Create profile:
+aws configure --profile 'name' [then input account keys and stuff]
+	profile credentials & congfig stored in c/users/'username'/.aws
+
+profile names: personal, ntpete
+
+To use a specific profile:
+aws 'service' 'command' --profile 'name'
+aws ec2 describe-instances --profile 'name'
+aws elasticbeanstalk describe-environments --profile 'name'
+
+Help:
+aws help
+aws help topics
+aws elasticbeanstalk help
+"""
+
+# EB workflow:
+# eb init --profile 'name'
+# eb create --profile 'name'
