5ff420cd08619cff72d391b24611878bd1e51078
Pete Friedhoff <pete.friedhoff@gmail.com>
2015-08-11 16:59:05 -0700

    update

--- postgresql/ALTER TABLES to add foreign keys.sql
+++ postgresql/ALTER TABLES to add foreign keys.sql
@@ -1,7 +1,16 @@
 ﻿-- alter tables after csv load to keep relationships
 
-ALTER TABLE changes_custom_attributes ADD FOREIGN KEY (change_guid) REFERENCES changes_summary (Guid);
-ALTER TABLE changes_lifecycle_history ADD FOREIGN KEY (change_guid) REFERENCES changes_summary (Guid);
-ALTER TABLE changes_decision_history ADD FOREIGN Key (change_guid) REFERENCES changes_summary (Guid);
-ALTER TABLE changes_items ADD FOREIGN KEY (change_guid) REFERENCES changes_summary (Guid);
-ALTER TABLE changes_requests ADD FOREIGN KEY (change_guid) REFERENCES changes_summary (Guid);
+ALTER TABLE changes_custom_attributes ADD FOREIGN KEY (change_guid) REFERENCES changes_summary (guid);
+ALTER TABLE changes_lifecycle_history ADD FOREIGN KEY (change_guid) REFERENCES changes_summary (guid);
+ALTER TABLE changes_decision_history ADD FOREIGN Key (change_guid) REFERENCES changes_summary (guid);
+ALTER TABLE changes_items ADD FOREIGN KEY (change_guid) REFERENCES changes_summary (guid);
+ALTER TABLE changes_requests ADD FOREIGN KEY (change_guid) REFERENCES changes_summary (guid);
+
+ALTER TABLE items_custom_attributes ADD FOREIGN KEY (item_guid)REFERENCES items_summary (guid);
+ALTER TABLE items_bom ADD FOREIGN KEY (parent_item_guid) REFERENCES items_summary (guid);
+ALTER TABLE items_bom ADD FOREIGN KEY (child_item_guid) REFERENCES items_summary (guid);
+ALTER TABLE items_sourcing ADD FOREIGN KEY (item_guid) REFERENCES items_summary (guid);
+
+ALTER TABLE projects_schedule ADD FOREIGN KEY (project_guid) REFERENCES projects_summary (guid);
+ALTER TABLE projects_referenced_items ADD FOREIGN KEY (project_guid) REFERENCES projects_summary (guid);
+ALTER TABLE projects_referenced_items ADD FOREIGN KEY (project_phase_task_guid) REFERENCES projects_schedule (phase_guid);
---
+++ postgresql/CREATE TABLE projects.sql
@@ -0,0 +1,56 @@
+﻿CREATE TABLE projects_summary
+(
+ tran varchar(5),
+ guid varchar(25) NOT NULL,
+ project_number varchar(100),
+ project_name varchar(200),
+ program varchar(100),
+ project_manager_full_name varchar(201),
+ project_manager_id varchar(100),
+ status varchar(40),
+ current_phase varchar(200),
+ description varchar(255),
+ start_date timestamp with time zone,
+ target_end_date timestamp with time zone,
+ completion_date timestamp with time zone,
+ creation_date timestamp with time zone,
+ template varchar(100),
+ comments varchar(255),
+ PRIMARY KEY (guid)
+);
+
+CREATE TABLE projects_schedule
+(
+ tran varchar(5),
+ guid varchar(25) NOT NULL,
+ project_guid varchar(25),
+ project_number varchar(100),
+ phase_guid varchar(25),
+ phase_name varchar(200),
+ task_guid varchar(25),
+ task_name varchar(200),
+ assignment_type varchar(40),
+ assignee_full_name varchar(201),
+ assignee_id varchar(100),
+ due_date timestamp with time zone,
+ completed_date timestamp with time zone,
+ status varchar(40),
+ PRIMARY KEY (guid)
+);
+
+CREATE TABLE projects_referenced_items
+(
+ tran varchar(5),
+ guid varchar(25) NOT NULL,
+ project_guid varchar(25),
+ project_number varchar(100),
+ project_phase_task_guid varchar(25),
+ project_phase_task_name varchar(200),
+ reference_type varchar(40),
+ item_guid varchar(25),
+ item_number varchar(200),
+ item_revision varchar(15),
+ PRIMARY KEY (guid)
+);
+
+ 
