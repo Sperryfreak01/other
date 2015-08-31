0866b23972815574750f41d72c1dee36641d0a68
Pete Friedhoff <pete.friedhoff@gmail.com>
2015-08-31 14:09:11 -0700

    api requests script update

--- API_requests_template.py
+++ API_requests_template.py
@@ -25,25 +25,35 @@ Google Directions API example
 """
 Arena PLM API
 """
+# Find all values of a given key for nested dict or list. Use with: list(find('guid', results_data))
+def find(key, dictionary):
+	for k, v in dictionary.items():
+		if k == key:
+			yield v
+		elif isinstance(v, dict):
+			for result in find(key, v):
+				yield result
+		elif isinstance(v, list):
+			for d in v:
+				for result in find(key, d):
+					yield result
+
 url = "https://api.arenasolutions.com/v1/"
 
-# Login first
-endpoint = 'login'
+# Login to get Arena session Id
 headers = {"content-type": "application/json"}
 body = {"email": "pfriedhoff@nextracker.com", "password": "", "workspaceId": "896561705"}
-r = requests.post(url + endpoint, headers = headers, data = json.dumps(body))
-login_data = r.json()
+r = requests.post(url + 'login', headers = headers, data = json.dumps(body))
+print ('Login code is: %s' % r.status_code)
 
-# Search for item object. Returns 'Compact Item' object
-endpoint = 'items'
-headers["arena_session_id"] = login_data["arenaSessionId"]
-params  = {'number': '5113'}
-r = requests.get(url + endpoint, headers = headers, params = params)
-item = r.json()
+# Search for Item based on number to get its GUID
+headers["arena_session_id"] = r.json().get('arenaSessionId')
+params  = {'number': 'PDM-000002'}
+r = requests.get(url + 'items', headers = headers, params = params)
+results_data = r.json()
 
-# Get item revisions. Returns 'revision' object
-item_guid = item['results'][0]['guid']
+print (list(find('guid', results_data)))
 
 # Logout
-# r = requests.get('https://api.arenasolutions.com/v1/logout', headers = headers)
-# print ('Logout code is: %s' % r.status_code)
+r = requests.get('https://api.arenasolutions.com/v1/logout', headers = headers)
+print ('Logout code is: %s' % r.status_code)
